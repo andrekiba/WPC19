7290f32311c9076902d9d52d765d47077e77b55a
Andrea <andre.kiba@gmail.com>
2019-11-24 18:18:50 +0100

    first commit

---
+++ code/WPC19/PollyRetry/Controllers/MicroBController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PollyRetry.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class MicroBController : ControllerBase
+	{
+		static int requestCount;
+		readonly ILogger<MicroAController> logger;
+
+		public MicroBController(ILogger<MicroAController> logger)
+		{
+			this.logger = logger;
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			await Task.Delay(100); 
+			requestCount++;
+
+			return requestCount % 3 == 0 ? 
+				Ok(6) : 
+				StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
+		}
+	}
+}
---
+++ code/WPC19/PollyRetry/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using PollyRetry.Services;
+using Refit;
+
+namespace PollyRetry.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddRefit(this IServiceCollection services, string baseAddress)
+		{
+			services.AddRefitClient<IMicroBApi>()
+				.ConfigureHttpClient(c => c.BaseAddress = new Uri(baseAddress));
+
+			return services;
+		}
+	}
+}
