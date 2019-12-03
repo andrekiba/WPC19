78807a68f7ec6caafafaf99c45b3abadc9701e73
Andrea <andrea.ceroni@gmail.com>
2019-12-03 05:30:57 +0100

    update

--- code/PollyCircuitBreaker/Controllers/AzureDevOpsController.cs
+++ code/PollyCircuitBreaker/Controllers/AzureDevOpsController.cs
@@ -5,7 +5,7 @@ using Api.Requests;
 using Api.Responses;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PollyCache.Controllers
+namespace PollyCircuitBreaker.Controllers
 {
 	[ApiController]
 	[Route("api/azuredevops")]
@@ -17,10 +17,9 @@ namespace PollyCache.Controllers
 		public async Task<IActionResult> GetProject(Guid id)
 		{
 			await Task.Delay(100);
-			
 			requestCount++;
 
-			if (requestCount % 2 == 0)
+			if (requestCount % 3 == 0)
 				return Ok(new Project
 				{
 					Id = id,
@@ -28,6 +27,18 @@ namespace PollyCache.Controllers
 				});
 
 			return StatusCode((int)HttpStatusCode.InternalServerError, "Something went wrong");
+		}
+
+		[HttpPost("projects")]
+		public async Task<IActionResult> CreateProject(CreateProject createProject)
+		{
+			await Task.Delay(100);
+
+			return Ok(new Project
+			{
+				Id = Guid.NewGuid(),
+				Name = createProject.Name
+			});
 		}
 	}
 }
---
+++ code/PollyCircuitBreaker/Startup.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Extensions.Http;
+using Refit;
+
+namespace PollyCircuitBreaker
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			IAsyncPolicy<HttpResponseMessage> retryPolicy = Policy
+				.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+				.RetryAsync(2, onRetry: (response, retryCount) =>
+				{
+					Debug.WriteLine($"Retry {retryCount}");
+				});
+
+			IAsyncPolicy<HttpResponseMessage> breakerPolicy = Policy
+				.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+				.CircuitBreakerAsync(2, TimeSpan.FromSeconds(30), OnBreak, OnReset, OnHalfOpen);
+
+			services.AddControllers();
+
+			services.AddRefitClient<IAzureDevOpsApi>()
+				.ConfigureHttpClient((serviceProvider, client) =>
+				{
+					client.BaseAddress = new Uri(Configuration["AppSettings:AzureDevOpsApiAddress"]);
+					client.DefaultRequestHeaders.Add("Accept", "application/json");
+				})
+				.AddPolicyHandler(retryPolicy)
+				
+				//DON'T DO THIS!! It creates a new circuit breaker instance per request --> most of the time is not what you want!
+				//.AddPolicyHandler(request => HttpPolicyExtensions.HandleTransientHttpError()
+				//	.CircuitBreakerAsync(2, TimeSpan.FromSeconds(60), OnBreak, OnReset, OnHalfOpen))
+				
+				//CORRECT --> Circuit Breaker is a stateful policy and can (most of the time must) be shared
+				.AddPolicyHandler(breakerPolicy);
+		}
+
+		static void OnHalfOpen()
+		{
+			Debug.WriteLine("Connection half open");
+		}
+
+		static void OnReset(Context context)
+		{
+			Debug.WriteLine("Connection reset");
+		}
+
+		static void OnBreak(DelegateResult<HttpResponseMessage> delegateResult, TimeSpan timeSpan, Context context)
+		{
+			Debug.WriteLine($"Connection break: {delegateResult.Result}, {delegateResult.Result}");
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
